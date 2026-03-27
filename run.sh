d3bdae25003ff4b83b8b872ca5c849069f8b8da0
Stephen Swensen <stephen.swensen@gmail.com>
2026-03-27 06:17:04 -0400

    flag support in run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 set -e
 
-if [ $# -eq 0 ]; then
-    echo "usage: ./run.sh file.fc [file2.fc ...]"
+fc_flags=()
+fc_files=()
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --flag)
+            fc_flags+=("--flag" "$2")
+            shift 2
+            ;;
+        *)
+            fc_files+=("$1")
+            shift
+            ;;
+    esac
+done
+
+if [ ${#fc_files[@]} -eq 0 ]; then
+    echo "usage: ./run.sh [--flag name ...] file.fc [file2.fc ...]"
     exit 1
 fi
 
@@ -12,7 +28,7 @@ make -s fc
 tmpdir=$(mktemp -d /tmp/fc-run.XXXXXX)
 trap 'rm -rf "$tmpdir"' EXIT
 
-./fc "$@" stdlib/* -o "$tmpdir/out.c"
+./fc "${fc_files[@]}" stdlib/* "${fc_flags[@]}" -o "$tmpdir/out.c"
 cc -std=c11 -Wall -Werror -o "$tmpdir/out" "$tmpdir/out.c"
 set +e
 "$tmpdir/out"
