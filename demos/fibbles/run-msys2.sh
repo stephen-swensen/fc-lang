9d27a57870316a93fe687e50fec47fcd83d2e7ed
Stephen Swensen <stephen.swensen@gmail.com>
2026-04-04 01:01:51 -0400

    Platform-conditional mkdir for Windows/MSYS2 support in stdlib/io

--- demos/fibbles/run-msys2.sh
+++ demos/fibbles/run-msys2.sh
@@ -7,7 +7,7 @@ set -e
 cd "$(dirname "$0")/../.."
 make -s fc
 OUTDIR="${TEMP:-/tmp}"
-./fc demos/fibbles/sdl.fc demos/fibbles/main.fc stdlib/io.fc stdlib/text.fc stdlib/sys.fc -o "$OUTDIR/fibbles.c"
+./fc demos/fibbles/sdl.fc demos/fibbles/main.fc stdlib/io.fc stdlib/text.fc stdlib/sys.fc --flag windows -o "$OUTDIR/fibbles.c"
 gcc -std=c11 -Wall -Werror -Dmain=SDL_main -o "$OUTDIR/fibbles.exe" "$OUTDIR/fibbles.c" -lmingw32 -lSDL2main -lSDL2 -lm
 echo "Running Fibbles..."
 "$OUTDIR/fibbles.exe"
