0b902b03d298aa6dcae0ca574a26f0835dea6e02
Stephen Swensen <stephen.swensen@gmail.com>
2026-04-06 22:46:08 -0400

    Replace C stdlib rand with pcg_random in Fibbles demo

--- demos/fibbles/run.sh
+++ demos/fibbles/run.sh
@@ -2,7 +2,7 @@
 set -e
 cd "$(dirname "$0")/../.."
 make -s fc
-./fc demos/fibbles/sdl.fc demos/fibbles/main.fc stdlib/io.fc stdlib/text.fc stdlib/sys.fc -o /tmp/fibbles.c
+./fc demos/fibbles/sdl.fc demos/fibbles/main.fc stdlib/io.fc stdlib/text.fc stdlib/sys.fc stdlib/random.fc -o /tmp/fibbles.c
 cc -std=c11 -Wall -Werror -o /tmp/fibbles-bin /tmp/fibbles.c -lSDL2 -lm
 echo "Running Fibbles..."
 /tmp/fibbles-bin
